76ebeef27e7120c0e4cad30a81adb1884fe3c2c4
gitx5622 <ggitau5622@gmail.com>
2020-11-28 14:20:00 +0300

    break

--- src/components/layout/MainSidebar/SidebarMainNavbar.js
+++ src/components/layout/MainSidebar/SidebarMainNavbar.js
@@ -40,7 +40,7 @@ class SidebarMainNavbar extends React.Component {
               />
               {!hideLogoText && (
                 <span className="d-none d-md-inline ml-1">
-                  Golang Dashboard
+                  Admin Template
                 </span>
               )}
             </div>
